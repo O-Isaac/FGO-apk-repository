2cfed35d42bed034b0b3bf5b824e851277350031
O-Isaac <zaragozamendozaisaac@gmail.com>
2023-05-28 16:52:43 +0200

    first commit

---
+++ depencencies.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo [+] Installing proto
+# Installation from https://grpc.io/docs/protoc-installation/
+PB_REL="https://github.com/protocolbuffers/protobuf/releases"
+curl -LO $PB_REL/download/v3.15.8/protoc-3.15.8-linux-x86_64.zip
+unzip protoc-3.15.8-linux-x86_64.zip -d $HOME/.local
+export PATH="$PATH:$HOME/.local/bin"
+rm protoc-3.15.8-linux-x86_64.zip
+
+echo [+] Installing protobuf
+pip install protobuf
+
+echo [+] Installing deps
+pip install -r googleplay-api/requirements.txt
